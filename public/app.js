260025996e7ec44c6e59b9f1db3d6da680643901
Daniel Duersteler <daniel.duersteler@gmail.com>
2020-12-06 02:43:13 +0100

    initial commit

--- public/app.js
+++ public/app.js
@@ -8,12 +8,13 @@ class randomSoup {
      * default values
      */
     defaults = {
-        sleepMilliseconds: 1000,
+        sleepMilliseconds: 100,
         fontFamilies: [
             'Arial',
             'Times',
             'Courier',
         ],
+        characters: '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ/*-=()&\*ç+°§?^![]$£{}àéèüöä|¢¬#@¦',
         colors: [
             '#00FF00',
             '#33FF33',
@@ -22,6 +23,35 @@ class randomSoup {
             '#999999',
             '#666666',
             '#333333',
+        ],
+        textShadows: [
+            '1px 1px 2px #FFFFFF',
+            '1px 1px 2px #00FF00',
+            '1px 1px 2px #33FF33',
+            '1px 1px 2px #FFFFFF',
+            '1px 1px 2px #CCCCCC',
+            '1px 1px 2px #999999',
+            '1px 1px 2px #666666',
+            '1px 1px 2px #333333',
+            'none',
+            'none',
+            'none',
+            'none',
+            'none',
+            'none',
+            'none',
+            'none',
+        ],
+        specialCharacters: '*',
+        specialColors: [
+            'red',
+            'gold',
+        ],
+        specialTextShadows: [
+            '1px 1px 2px red',
+            '1px 1px 2px gold',
+            'none',
+            'none',
         ],
         containerElementId: 'randomSoupContainer',
         maxCycles: 1000,
@@ -52,42 +82,58 @@ class randomSoup {
      */
     drawCycle() {
 
-        // if max cycles are not reached
-        if (this.counter < this.settings.maxCycles) {
+        // create dom element
+        let element = document.createElement('span');
 
-            // values for new element
-            let char = this.randomString();
-            let fontSize = this.randomInteger(12, 48);
-            let marginTop = this.randomInteger(0, window.innerHeight);
-            let marginLeft = this.randomInteger(0, window.innerWidth);
-            let fontFamily = this.settings.fontFamilies[this.randomInteger(0, this.settings.fontFamilies.length - 1)];
-            let color = this.settings.colors[this.randomInteger(0, this.settings.colors.length - 1)];
+        // element styles and content
+        let char;
+        let fontSize;
+        let marginTop;
+        let marginLeft;
+        let fontFamily;
+        let textShadow;
+        let color;
 
-            // create dom element
-            let element = document.createElement('span');
+        // values for new element
+        char = this.randomString(1, this.settings.characters);
+        fontSize = this.randomInteger(12, 48);
+        marginTop = this.randomInteger(0, window.innerHeight);
+        marginLeft = this.randomInteger(0, window.innerWidth);
+        fontFamily = this.settings.fontFamilies[this.randomInteger(0, this.settings.fontFamilies.length - 1)];
+        textShadow = this.settings.textShadows[this.randomInteger(0, this.settings.textShadows.length - 1)];
+        color = this.settings.colors[this.randomInteger(0, this.settings.colors.length - 1)];
+        
+        // overwrite in special cases ...
+        if (this.randomInteger(0, this.maxCycles) === this.maxCycles) {
+            char = this.randomString(1, this.settings.specialCharacters);
+            fontSize = 200;
+            textShadow = this.settings.specialTextShadows[this.randomInteger(0, this.settings.specialTextShadows.length - 1)];
+            color = this.settings.specialColors[this.randomInteger(0, this.settings.specialColors.length - 1)];
+        }
 
-            // set dom element style
-            element.style.marginTop = '' + marginTop + 'px';
-            element.style.marginLeft = '' + marginLeft + 'px';
-            element.style.fontSize = '' + fontSize + 'px';
-            element.style.fontFamily = '' + fontFamily + 'px';
-            element.style.color = '' + color;
-            element.style.position = 'absolute';
+        // set element content
+        element.textContent = char;
 
-            // set element content
-            element.textContent = char;
+        // set dom element style
+        element.style.marginTop = '' + marginTop + 'px';
+        element.style.marginLeft = '' + marginLeft + 'px';
+        element.style.fontSize = '' + fontSize + 'px';
+        element.style.fontFamily = '' + fontFamily + 'px';
+        element.style.color = '' + color;
+        element.style.position = 'absolute';
+        element.style.textShadow = textShadow;
 
-            // append element to container
-            document.getElementById(this.settings.containerElementId).append(element);
+        // append element to container
+        document.getElementById(this.settings.containerElementId).append(element);
 
-            // increment counter
-            this.counter++;
+        // increment counter
+        this.counter++;
 
         // begin cleaning up, when max cycles are reached
-        } else {
+        if (this.counter > this.settings.maxCycles) {
 
             // remove oldest element
-            let element = document.getElementsByTagName('span')[0];
+            element = document.getElementsByTagName('span')[0];
             element.remove();
 
             // decrement counter
@@ -127,7 +173,10 @@ class randomSoup {
     }
 }
 
-// create random soup instance
+/**
+ * create random soup instance with custom options
+ */
 let randomSoup1 = new randomSoup({
     sleepMilliseconds: 1000,
+    maxCycles: 3,
 });
