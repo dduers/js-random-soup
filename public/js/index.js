a26fcbdcee32285dbb41ba3d304cc76e1bb4907b
Daniel Duersteler <daniel.duersteler@gmail.com>
2020-12-08 01:29:19 +0100

    unified forms for the two modes stripes and screen

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,17 @@
 /**
  * init input fields
  */
-window.addEventListener('resize', function(event) {
-    document.getElementById('stripeCount1').value = Math.ceil(window.innerWidth / 20);
-    document.getElementById('maxCycles1').value = Math.ceil(window.innerHeight / 16);
-    document.getElementById('maxCycles2').value = Math.ceil(window.innerHeight * window.innerWidth / 10000);
-});
 
-document.getElementById('stripeCount1').value = Math.ceil(window.innerWidth / 20);
-document.getElementById('maxCycles1').value = Math.ceil(window.innerHeight / 16);
-document.getElementById('maxCycles2').value = Math.ceil(window.innerHeight * window.innerWidth / 10000);
+function updateParameterValues() {
+    document.getElementById('stripeCount1').value = Math.ceil(window.innerWidth / 20);
+    if (document.getElementById('type').value == 'stripe') {
+        document.getElementById('maxCycles1').value = Math.ceil(window.innerHeight / 16);
+    } else {
+        document.getElementById('maxCycles1').value = Math.ceil(window.innerHeight * window.innerWidth / 10000);
+    }
+}
+
+window.addEventListener('resize', updateParameterValues);
+document.getElementById('type').addEventListener('change', updateParameterValues);
+
+updateParameterValues();
