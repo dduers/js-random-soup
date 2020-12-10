9dc04c8ded85dff5409d0ab8d2c8243ea664a18d
Daniel Duersteler <daniel.duersteler@gmail.com>
2020-12-10 23:40:04 +0100

    updated with overlay mode

--- public/js/index.js
+++
@@ -1,9 +0,0 @@
-// update parameters
-function updateParameterValues() {
-    document.getElementById('stripeCount1').value = Math.ceil(window.innerWidth / 100);
-    document.getElementById('maxCycles1').value = Math.ceil(window.innerHeight * window.innerWidth / 50000);
-}
-
-// update parameters
-window.addEventListener('resize', updateParameterValues);
-updateParameterValues();
